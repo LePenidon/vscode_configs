0f29669ceca229fdb3bcf9418f0d0a13421c5efa
lepenidon <penido1000@gmail.com>
2025-05-23 11:16:01 -0300

    config

--- linux/configs.sh
+++ linux/configs.sh
@@ -1,3 +1,7 @@
+#bash config.sh
+
+
 gsettings set org.nemo.preferences show-image-thumbnails 'always'
 echo 'xinput set-prop 12 "libinput Scrolling Pixel Distance" 30' >> ~/.xprofile
 dconf load /org/gnome/terminal/ < /home/penidonot/Documents/git_workspace/vscode_configs/linux/teminal_config/terminal_backup.dconf
+dconf write /org/gnome/gnome-screenshot/auto-save-directory "'file:///home/$USER/Pictures/Screenshots'"
