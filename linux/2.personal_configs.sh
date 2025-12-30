da470716ac589ed53e3fb600a13fd8141a25e891
lepenidon <penido1000@gmail.com>
2025-12-30 08:34:43 -0300

    configs

--- linux/2.personal_configs.sh
+++ linux/2.personal_configs.sh
@@ -1,11 +1,16 @@
 #bash config.sh
 
-sudo prime-select intel
 gsettings set org.nemo.preferences show-image-thumbnails 'always'
 gsettings set org.gnome.nautilus.preferences click-policy 'double'
+
 echo 'xinput set-prop 12 "libinput Scrolling Pixel Distance" 30' >> ~/.xprofile
+
 dconf load /org/gnome/terminal/ < /home/penidonot/Documents/git_workspace/vscode_configs/linux/teminal_config/terminal_backup.dconf
+
 dconf write /org/gnome/gnome-screenshot/auto-save-directory "'file:///home/$USER/Pictures/Screenshots'"
+
+gsettings set org.gnome.desktop.interface cursor-size 48
+sudo sed -i 's/Size=[0-9]*/Size=48/' /usr/share/icons/default/index.theme
 
 mkdir -p ~/.local/share/fonts
 wget -P ~/.local/share/fonts "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip"
